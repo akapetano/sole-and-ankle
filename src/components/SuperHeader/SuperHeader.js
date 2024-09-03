0c7222d23d1231ab6d4959d7148263af87a1a575
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2024-09-03 14:31:49 +0200

    finish superheader exercise

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import SearchInput from '../SearchInput';
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
+import SearchInput from "../SearchInput";
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
 
 const SuperHeader = () => {
   return (
@@ -23,13 +23,23 @@ const SuperHeader = () => {
 };
 
 const Wrapper = styled.div`
+  display: none;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    gap: ${24 / 16}rem;
+    padding-block: ${12 / 16}rem;
+    padding-inline: ${32 / 16}rem;
+  }
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-right: auto;
 `;
 
 const HelpLink = styled.a`
